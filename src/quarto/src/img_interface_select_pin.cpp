cde30d71c5cf21642247195f21626ade977ea10b
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-06-08 16:24:48 +0900

    Add function paste for other picture

--- src/quarto/src/img_interface_select_pin.cpp
+++ src/quarto/src/img_interface_select_pin.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <ros/ros.h>
-
+#include <bitset>
 #include <thread>
 #include <chrono>
+
+#include <ros/ros.h>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
@@ -12,10 +13,28 @@
 
 #include "quarto/bridge.h"
 
-int global_pin = 0;
+int global_pin = 10;//parfect not exist
 int before_pin = global_pin;
+
 int width = 0;
 int height = 0;
+std::bitset<9> isexist("111111111");
+struct pos{
+  int x;
+  int y;
+  int width;
+  int height;
+};
+
+std::vector<struct pos> vec(9);
+
+/*void check_isexist()
+{
+  for (int i {};i < 8;++i) {
+    std::cout << ((isexist[i] == true)?1:0);
+  }
+  std::cout << ' ';
+}*/
 
 void callback_mouse(int event, int x, int y, int flags, void*)
 {
@@ -27,11 +46,51 @@ void callback_mouse(int event, int x, int y, int flags, void*)
       break;
   }
 }
+/*
+void paste_mat_img(cv::Mat src, cv::Mat dst, int x, int y, int copy_width, int copy_height) {
+  std::cout << "check\n";
+  cv::Mat resized_img;
+  cv::resize(src, resized_img, cv::Size(copy_width, copy_height));
+  //cv::Mat roi(dst, cv::Rect(y, x, copy_height, copy_width));
+  cv::Mat roi= dst(cv::Rect(y, x, copy_height, copy_width));
+  resized_img.copyTo(roi);
+}
+*/
+// 画像を画像に貼り付ける関数
+void paste_mat_img(cv::Mat src, cv::Mat dst, int x, int y, int resize_width, int resize_height) {
+  std::cout << "paste\n";
+	cv::Mat resized_img;
+	cv::resize(src, resized_img, cv::Size(resize_width, resize_height));
 
-const std::string pin_box[] = {"0", "1", "2", "3","4","5","6","7","8","9"};
+	if (x >= dst.cols || y >= dst.rows) return;
+	int w = (x >= 0) ? std::min(dst.cols - x, resized_img.cols) : std::min(std::max(resized_img.cols + x, 0), dst.cols);
+	int h = (y >= 0) ? std::min(dst.rows - y, resized_img.rows) : std::min(std::max(resized_img.rows + y, 0), dst.rows);
+	int u = (x >= 0) ? 0 : std::min(-x, resized_img.cols - 1);
+	int v = (y >= 0) ? 0 : std::min(-y, resized_img.rows - 1);
+	int px = std::max(x, 0);
+	int py = std::max(y, 0);
+
+	cv::Mat roi_dst = dst(cv::Rect(px, py, w, h));
+	//cv::Mat roi_dst = dst(cv::Rect(x_start, y_start, resize_width, resize_height));
+	cv::Mat roi_resized = resized_img(cv::Rect(u, v, w, h));
+	//cv::Mat roi_resized = resized_img(cv::Rect(- x_start, - y_start, resize_width, resize_height));
+	roi_resized.copyTo(roi_dst);
+}
+/*
+struct pos{
+  int x;
+  int y;
+  int width;
+  int height;
+};
+*/
+void paste_mat_img(cv::Mat src, cv::Mat dst, const struct pos* select) {
+  paste_mat_img(src, dst, select->x, select->y, select->width ,select->height);
+}
+
+const std::string pin_box[] = {"first", "second", "third", "fourth","fifth","sixth","seventh","eighth","ninth"};
 
 int main(int argc, char** argv){
-
   ros::init(argc, argv, "quarto_select_pin_client");
 
   ros::NodeHandle nh;
@@ -41,10 +100,43 @@ int main(int argc, char** argv){
     std::cout << "error\n";
   }
 
-  cv::Mat img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/test.jpg", cv::IMREAD_COLOR);
-
+  cv::Mat img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/temp.png", cv::IMREAD_COLOR);
   width = img_src.size().width;
   height = img_src.size().height;
+  for (int i {};i < 3;++i) {
+    for (int j {}; j < 3;++j) {
+      vec[i + j].x = (width* j) / 3;
+      vec[i + j].y = (height*i) / 3;
+      vec[i + j].width =  width/3;
+      vec[i + j].height = height / 3;
+    }
+  }
+/*
+  const cv::Mat one_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/one.png");
+  paste_mat_img(one_img_src, img_src, 0, 0, width/3, height/ 3);
+  const cv::Mat two_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/two.png");
+  paste_mat_img(two_img_src, img_src, width/3 , 0, width/3 , height/ 3);
+  const cv::Mat three_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/three.png");
+  paste_mat_img(three_img_src, img_src, 2*width/3, 0, width/3, height/3);
+
+  const cv::Mat four_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/four.png");
+  paste_mat_img(four_img_src, img_src, 0, height/3, width/3, height/3);
+  const cv::Mat five_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/five.jpg");
+  paste_mat_img(five_img_src, img_src, width/3,height/3,width/3, height/ 3);
+  const cv::Mat six_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/six.jpg");
+  paste_mat_img(six_img_src, img_src, 2*width/3, height/3, width/3, height/ 3);
+  
+  const cv::Mat seven_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/seven.png");
+  paste_mat_img(seven_img_src, img_src, 0, 2*height/3, width/3, height/3);
+  const cv::Mat eight_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/eight.png");
+  paste_mat_img(eight_img_src, img_src, width/3, 2*height/3, width/3, height/ 3);
+  const cv::Mat nine_img_src = cv::imread("/home/tamura-kosei/works/board_game_ros/src/quarto/img/nine.png");
+  paste_mat_img(nine_img_src, img_src, 2*width/3,2*height/3,  width/3, height/3);
+
+  cv::imwrite("/home/tamura-kosei/works/board_game_ros/src/quarto/img/temp.png", img_src);
+*/
+  cv::Mat image_blank(640, 480, CV_16U);
+  cv::imshow("img_blank", image_blank);
   ROS_INFO("START %s","select_pin" );
   ROS_INFO("%d %d", width, height);
 
@@ -60,25 +152,55 @@ int main(int argc, char** argv){
   quarto::bridge srv;
   srv.request.str_pin = '0';// = pin_box[global_pin];
   ros::ServiceClient client = nh.serviceClient<quarto::bridge>("select_pin");
-  /*
-  while(cv::waitKey(1) != 'q') {
-    if(global_pin != before_pin && 0 <= global_pin && global_pin <= 9) {
-      ROS_INFO("%d" , global_pin);
-    }
-  }
-  */
-
-  //ros::Rate r(1);
-
-  //ros::ServiceClient client = nh.serviceClient<quarto::bridge>("select_pin");
 
   while(cv::waitKey(1) != 'q'){
 
     if(global_pin != before_pin){
-      if(0 <= global_pin && global_pin <= 9){
+      if(0 <= global_pin && global_pin < 9 && isexist[global_pin] == true){
+        if (global_pin == 0)paste_mat_img(image_blank, img_src, 0, 0, width/3, height/ 3);
+        switch(global_pin) {
+          default:
+            ROS_INFO("aDD SOME");
+            break;
+          case 0:
+            paste_mat_img(img_src, img_src, 0, 0, width/3, height/ 3);
+            break;
+          case 1:
+            paste_mat_img(img_src, img_src, width/3 , 0, width/3 , height/ 3);
+            break;
+          case 2:
+            paste_mat_img(image_blank, img_src, 2*width/3, 0, width/3, height/3);
+            break;
+          case 3:
+            paste_mat_img(image_blank, img_src, 0, height/3, width/3, height/3);
+            break;
+          case 4:
+            paste_mat_img(image_blank, img_src, width/3,height/3,width/3, height/ 3);
+            break;
+          case 5:
+            paste_mat_img(image_blank, img_src, 2*width/3, height/3, width/3, height/ 3);
+            break;
+          case 6:
+            paste_mat_img(image_blank, img_src, 0, 2*height/3, width/3, height/3);
+            break;
+          case 7:
+            paste_mat_img(image_blank, img_src, width/3, 2*height/3, width/3, height/ 3);
+            break;
+          case 8:
+            paste_mat_img(image_blank, img_src, width/3, 2*height/3, width/3, height/ 3);
+            break;
+          case 9:
+            paste_mat_img(image_blank, img_src, 2*width/3,2*height/3,  width/3, height/3);
+            break;
+        }
         srv.request.str_pin = pin_box[global_pin];
-        ROS_INFO("Push to server from selectPin client");
+        ROS_INFO("PUSH REQUEST");
+        //paste_mat_img(image_blank, img_src, &vec[global_pin]);
+
+        ROS_INFO("CHECK");
+
         client.call(srv);
+        isexist.reset(global_pin);
       }
       /*
 
@@ -93,11 +215,8 @@ int main(int argc, char** argv){
     }
 
 
-    //ros::spin();
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    //r.sleep();
   }
-
   ros::spin();
   return 0;
 }
