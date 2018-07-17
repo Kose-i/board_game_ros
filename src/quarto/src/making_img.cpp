f1b7a38d8e82859e6cedf988f9eeeb989d3f3e6e
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-07-17 15:12:54 +0900

    Modified making img

--- src/quarto/src/making_img.cpp
+++ src/quarto/src/making_img.cpp
@@ -19,7 +19,7 @@ struct Plot_data{
 };
 
 int main(int argc, char** argv){
-  cv::Mat target_img = cv::Mat::zeros(480, 640, CV_8UC3);
+  cv::Mat target_img = cv::Mat::zeros(640, 480, CV_8UC3);
   cv::imwrite("/home/tamura-kosei/works/board_game_ros/src/quarto/img/blank_img.png", target_img);
   /*
   cv::putText(target_img, "OpenCV", cv::Point(50, 50),cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 0, 255), 2, CV_AA);
@@ -29,7 +29,7 @@ int main(int argc, char** argv){
   std::vector<struct Plot_data> pack_data(9);
   for (int i {};i < 7;++i) {
     pack_data[i].output_c = '1' + i;
-    pack_data[i].left_up = cv::Point(100*(i%3 + 1), 100*(i/3 + 1));
+    pack_data[i].left_up = cv::Point((480/4)*(i%3 + 1), (640/4)*(i/3 + 1));
     pack_data[i].right_down = cv::Point();
     pack_data[i].color = cv::Scalar(255, 255, 255);//white color
   }
